e36f52307c75bebb533887a46f9951b9fa717438
Shivam <sriyanshshivam1@gmail.com>
2025-02-23 15:00:50 +0530

    Add structure for backend

--- backend-trackit/main.go
+++ backend-trackit/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-    "fmt"
     "log"
     "net/http"
     "os"
     "github.com/gin-gonic/gin"
-    "github.com/joho/godotenv"
-    "task-management/internal/handlers"
-    "task-management/internal/middleware"
-    "task-management/internal/database"
 )
 
 func main() {
