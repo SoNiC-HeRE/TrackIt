7ac35b3d66c50075488b717aea050dc3b703358f
Shivam <sriyanshshivam1@gmail.com>
2025-02-23 14:50:23 +0530

    Add modules for backend

--- backend-trackit/main.go
+++ backend-trackit/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
-
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "github.com/gin-gonic/gin"
+    "github.com/joho/godotenv"
+    "task-management/internal/handlers"
+    "task-management/internal/middleware"
+    "task-management/internal/database"
+)
 func main() {
     fmt.Println("Hello, Go!")
 }
